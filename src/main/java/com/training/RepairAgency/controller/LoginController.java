27372bd9cfd01726e7351dbcbccd9d38bd689abf
Me <m.sadovskiy87@gmail.com>
2019-11-26 17:16:53 +0200

    Minor fix

--- src/main/java/com/training/RepairAgency/controller/LoginController.java
+++ src/main/java/com/training/RepairAgency/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.training.RepairAgency.controller;
 
-import com.training.RepairAgency.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
